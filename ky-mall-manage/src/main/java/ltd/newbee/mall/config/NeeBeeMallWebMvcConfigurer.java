cfbb288c31f283b72873314cb975fd48ae1a8a42
Pridelory <wangmeng.pantera@gmail.com>
2020-10-13 11:18:04 +0800

    first commit

--- ky-mall-manage/src/main/java/ltd/newbee/mall/config/NeeBeeMallWebMvcConfigurer.java
+++ ky-mall-manage/src/main/java/ltd/newbee/mall/config/NeeBeeMallWebMvcConfigurer.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class NeeBeeMallWebMvcConfigurer implements WebMvcConfigurer {
 
