1816a7f68c5a227ebb088b5142c3c0bc215f0e28
Pridelory <wangmeng.pantera@gmail.com>
2020-11-20 00:42:08 +0800

    简要配置spring cloud gateway

--- ky-mall-gateway/src/main/java/com/wangmeng/MallGatewayApplication.java
+++ ky-mall-gateway/src/main/java/com/wangmeng/MallGatewayApplication.java
@@ -1,14 +1,17 @@
 package com.wangmeng;
 
-import com.wangmeng.config.IgnoreUrlsConfig;
+//import com.wangmeng.config.IgnoreUrlsConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableConfigurationProperties(IgnoreUrlsConfig.class)
+//@EnableConfigurationProperties(IgnoreUrlsConfig.class)
 @EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class MallGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallGatewayApplication.class, args);
